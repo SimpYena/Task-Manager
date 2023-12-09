1e793a95e4da5947f40ed5008016e32b4686921f
SimpYena <jointhe611@gmail.com>
2023-12-09 17:59:01 +0700

    set up connect to mongo

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const route = require('./route/tasks')
 const port = 3000
+require('dotenv').config()
+const connectDB = require('./db/connect')
 //middleware
 app.use(express.json())
 app.get('/',(req,res)=>{
@@ -10,6 +12,14 @@ app.get('/',(req,res)=>{
 
 app.use('/api/v1/tasks', route)
 
-app.listen(port, ()=>{
-    console.log(`Server is listening on ${port}`);
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, ()=>{
+            console.log(`Server is listening on ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
